3b4d3e14971eb9492e1deb7ca4838004dae53ce5
chengyu zhang <ton_nom@163.com>
2023-08-06 18:27:42 +0800

    add uniprot id instead of replacement

--- add_uniprot_id/addUniprotID.m
+++ add_uniprot_id/addUniprotID.m
@@ -4,10 +4,23 @@ cd ../add_uniprot_id/
 fid = fopen('SGD_with_Uniprot.csv');
 uniprot = textscan(fid,'%q %q %q','Delimiter',',','HeaderLines',1);
 for i = 1:length(model.genes)
-    ids.name{1, 1} = char("Uniprot ID");
-    ind = find(strcmp(model.genes{i, 1}, uniprot{1, 1}));
-    ids.value{1, 1} = uniprot{1, 3}{ind, 1};
-    model.geneMiriams{i, 1} = ids;
+    gM = model.geneMiriams(i);
+    gM = gM{1,1};
+    if ~isa(gM, 'struct')
+        ids.name{1, 1} = char('kegg.genes');
+        ids.name{2, 1} = char('ncbigene');
+        ids.name{3, 1} = char('refseq');
+        ids.name{4, 1} = char('uniprot');
+        ids.name{5, 1} = char('ncbiprotein');
+        ind = find(strcmp(model.genes{i, 1}, uniprot{1, 1}));
+        ids.value{4, 1} = uniprot{1, 3}{ind, 1};
+        ids.value{1, 1} = char('NaN');
+        ids.value{2, 1} = char('NaN');
+        ids.value{3, 1} = char('NaN');
+        ids.value{5, 1} = char('NaN');
+        model.geneMiriams{i, 1} = ids;
+        clear ids
+    end
 end
 cd ../code/
 saveYeastModel(model)
