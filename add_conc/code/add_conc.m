9bbc2ae664e97ca69524d5a6b0ead9bb37bd2bf5
chengyu zhang <ton_nom@163.com>
2024-03-06 19:43:41 +0800

    Update add_conc.m

--- add_conc/code/add_conc.m
+++ add_conc/code/add_conc.m
@@ -1,3 +1,4 @@
+% load model and YMDB data
 modelWithConc = importModel('../../model/yeast-GEM.xml');
 data = readtable('../data/allConcData.xlsx');
 data = table2cell(data);
@@ -7,7 +8,6 @@ metConc.minConc = cell(length(modelWithConc.metNames), 1);
 
 met.name = modelWithConc.metNames;
 met.kegg = {};
-
 for i = 1:length(met.name)
     idx = find(strcmp('kegg.compound',modelWithConc.metMiriams{i, 1}.name));
     if idx
@@ -16,12 +16,12 @@ for i = 1:length(met.name)
     clear idx
 end
 
+% add conc into model
 metWithConc = {};
 for j = 2:870
     keggId = data{2, j};
     idx = find(strcmp(keggId, met.kegg));
     if idx
-        %fprintf(keggId)
         metWithConc{end+1} = keggId;
         for id= 1:length(idx)
             metConc.allConc{idx(id), 1} = data{3, j};
