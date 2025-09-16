d00a09d038bf329903dd192203f5a84edd3076e0
Eduard Kerkhoven <eduardk@chalmers.se>
2025-09-16 21:07:10 +0200

    feat: functions run from matlab path

--- code/otherChanges/fitGAM.m
+++ code/otherChanges/fitGAM.m
@@ -4,8 +4,11 @@
 
 function model = fitGAM(model)
 
+funcDir = dbstack('-completenames');
+funcDir = regexprep(funcDir(1).file,[funcDir(1).name '\.m'],'')
+
 %Load chemostat data:
-fid = fopen('../../data/physiology/chemostatData_VanHoek1998.tsv','r');
+fid = fopen(fullfile(funcDir,'../../data/physiology/chemostatData_VanHoek1998.tsv','r'));
 exp_data = textscan(fid,'%f32 %f32 %f32 %f32','Delimiter','\t','HeaderLines',1);
 exp_data = [exp_data{1} exp_data{2} exp_data{3} exp_data{4}];
 fclose(fid);
