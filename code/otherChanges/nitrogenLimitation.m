9676af17ff16914bef2329a25ae9900b356f10c2
Eduard Kerkhoven <eduardk@chalmers.se>
2026-07-01 01:08:09 +0200

    fix: correct constraints nLimit and GCV functions

--- code/otherChanges/nitrogenLimitation.m
+++ code/otherChanges/nitrogenLimitation.m
@@ -10,7 +10,7 @@ function model = nitrogenLimitation(model)
 % Glutamine synthase is repressed when nitrogen is in excess. See doi:10.1128/EC.2.5.827-829.2003
 model.ub(strcmp(model.rxns,'r_0472'))=1000;
 % Glycine cleavage system is repressed when nitrogen (non-glycine) is in excess
-model.lb(strcmp(model.rxns,'r_0501'))=1000; %glycine cleavage, mitochondrion
-model.lb(strcmp(model.rxns,'r_0507'))=1000; %glycine cleavage complex (lipoylprotein), mitochondrion
-model.lb(strcmp(model.rxns,'r_0509'))=1000; %glycine cleavage complex (lipoamide), mitochondrion
+model.ub(strcmp(model.rxns,'r_0501'))=1000;
+model.ub(strcmp(model.rxns,'r_0507'))=1000;
+model.ub(strcmp(model.rxns,'r_0509'))=1000;
 end
